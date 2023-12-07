529190ee49b23595934d2edc7ee19723a9daa93f
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-12-07 13:57:22 +0100

    Add project files.

---
+++ MusicStore.NetFramework.WebApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MusicStore.NetFramework.WebApp.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
